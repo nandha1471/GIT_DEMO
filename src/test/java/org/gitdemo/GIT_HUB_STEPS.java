33886d656babd7ff6fa7e3761254e4d454160d8c
nandha1471 <nandha.backup.ac@gmail.com>
2022-08-27 15:16:55 +0530

    step_1

---
+++ src/test/java/org/gitdemo/GIT_HUB_STEPS.java
@@ -0,0 +1,19 @@
+package org.gitdemo;
+
+public class GIT_HUB_STEPS {
+	
+	private void gitInitiation() {
+
+		String gitStart = " git config --global user.email gitemailID"
+				+         " git config --global user.name gitusername"
+				+         " git init"
+				+         " git status"
+				+         " git add ."
+				+         " git commit -m your message "
+				+         " git remote -v"
+				+         " git remote remove origin"
+				+         " git remote add origin https://token@github.com/username/repositoryname.git"
+				+         " git push -u origin master";
+		}
+
+}
